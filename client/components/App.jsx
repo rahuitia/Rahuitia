028b51c18a0f8e40619e204630faf22ec9fb20c8
blaineanthonywestern <blaine.a.western@gmail.com>
2019-07-10 15:27:06 +1200

    change config

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,12 +5,9 @@ import { fetchAllRahui } from "../actions/rahui";
 
 import Landing from "./Landing/Landing";
 import Nav from "./Nav/Nav";
-
 import Explore from "./Explore/Explore";
-
 import RahuiDetail from "./RahuiDetail/RahuiDetail";
 import About from "./About/About";
-
 import Login from "./Login/Login";
 import Add from "./Add/FormAndMap";
 import Register from "./Register/Register";
@@ -40,7 +37,7 @@ class App extends Component {
         <Route exact path="/about" component={About} />
         <Route exact path="/login/" component={Login} />
         <Route exact path="/AddRahui" component={Add} />
-        <Route exact path="/rahui/:id/edit" component={Edit}/>       
+        <Route exact path="/rahui/:id/edit" component={Edit}/>      
         <Route exact path="/explore" component={Explore} />
         <Route exact path="/rahui/:id" component={RahuiDetail} />
         <Route path="/register" component={Register} />
